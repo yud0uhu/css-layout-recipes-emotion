090658ec0d3d2f4c55b3c2e48ed157fb81946e29
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-10-03 22:09:33 +0900

    feat: @storybook/builder-viteのセットアップ

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const argTypes = {
+  globalArg: { options: ["A", "B"], control: "select" },
+};
+
+export const args = { globalArg: "A" };
