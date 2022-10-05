781c9ca70384fd35403a31db8f16081d1179547d
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-10-06 00:43:30 +0900

    feat: フレックスボックスを作成

---
+++ src/components/templates/column/flex-box/__asset/style/index.ts
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  border: 2px solid rgb(75, 70, 74);
+  border-radius: 0.5em;
+  padding: 20px 10px;
+  display: flex;
+  padding: 10px;
+  border: 2px solid rgb(95, 97, 110);
+  border-radius: 0.5em;
+  margin: 0 10px;
+  flex: 1;
+`;
+
+// NOTE: Container要素の直下にある(子要素である)全称セレクタにスタイルが適用される;
+export const Wrapper = styled.div`
+  & ${Container} > * {
+    padding: 10px;
+    border: 2px solid rgb(95, 97, 110);
+    border-radius: 0.5em;
+    margin: 0 10px;
+    flex: 1;
+  }
+`;
