b2bb78d89c0ca71f4dda461e3fb980107fe1fdbd
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-10-05 12:40:00 +0900

    fix: グローバルcssを全体に適用

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import MediaComponent from "./components/MediaComponent";
+import Media from "./components/templates/media";
 import "./index.css";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     {/* <App /> */}
-    <MediaComponent />
+    <Media />
   </React.StrictMode>
 );
